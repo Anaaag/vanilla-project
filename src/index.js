0ec63410ea1aff311f4ed45067848047c01b8e6b
Anaaag <agarc20@outlook.com>
2024-03-31 19:33:46 -0700

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -2,37 +2,58 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#weather-city-name");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
+  console.log(response.data.condition.description);
 
-  cityElement.innerHTML = response.data.city
-  temperatureElement.innerHTML = Math.round(temperature)
+  cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  temperatureElement.innerHTML = Math.round(temperature);
 }
 
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  let day = days[date.getDay()];
 
+  if (minutes < 10) {
+    minutes = `0${minutes}`
+  }
 
-
-
-
+  return `${day} ${hours}:${minutes}`
+}
 
 function searchCity(city) {
-let apiKey = "ab9d5a54441t2501e86dfdb9a436be3o"
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}}&key=${apiKey}&units=metric`;
-axios.get(apiUrl).then(refreshWeather)
-
+  let apiKey = "ab9d5a54441t2501e86dfdb9a436be3o";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
 }
-
-
 
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
- 
+
   searchCity(searchInput.value);
 }
 
-
-
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit)
+searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Vancouver");
